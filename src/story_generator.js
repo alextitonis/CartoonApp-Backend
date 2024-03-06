6657d714a7356d10cdd8ab2129722ad546388dd3
alextitonis <artificialleap12@gmail.com>
2024-03-06 13:13:56 +0300

    u

--- src/story_generator.js
+++ src/story_generator.js
@@ -57,8 +57,8 @@ export default async function generateStory(
     panels.push(panel);
   }
 
-  if (panels.length >= 3) {
-    panels.splice(3);
+  if (panels.length >= 2) {
+    panels.splice(2);
   }
 
   console.log("Total panels:", panels.length);
